4c6f17d5dc40c01884d3d29522ddc1323228294b
Priyanka Madhwal <priyanka0701m@gmail.com>
2018-09-10 20:51:44 +0530

    Create 1- rightTriangle.py

---
+++ 1- rightTriangle.py
@@ -0,0 +1,20 @@
+def rightTriangle():
+    '''
+    Objective:  To print a right triangle of n asterisks.
+                It comprises of four lines.
+    '''
+    # Approach: Use a print statement for each line of output.
+    print('*')
+    print('**')
+    print('***')
+    print('****')
+
+def main():
+    '''
+    Objective: To print a right triangle of n asterisks comprising of four lines.
+    '''
+    #Approach: Use rightTriangle() function.
+    rightTriangle()
+
+if __name__=='__main__':
+    main()
