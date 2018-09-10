1bfd8fc47f0ee671c28c97474131ed4925c07c11
Priyanka Madhwal <priyanka0701m@gmail.com>
2018-09-10 20:55:47 +0530

    Create 2- equiTriangle.py

---
+++ 2- equiTriangle.py
@@ -0,0 +1,20 @@
+def equiTriangle():
+    '''
+    Objective:  To print an equilateral triangle of n asterisks.
+                It comprises of four lines.
+    '''
+    # Approach: Use a print statement for each line of output.
+    print('   *   ')
+    print('  ***  ')
+    print(' ***** ')
+    print('*******')
+
+def main():
+    '''
+    Objective: To print an equilateral triangle of n asterisks comprising of four lines.
+    '''
+    #Approach: Use equiTriangle() function.
+    equiTriangle()
+
+if __name__=='__main__':
+    main()
