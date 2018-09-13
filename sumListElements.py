689ecda67c5f2afd8cbd0e72b94b168c338d1c80
Priyanka Madhwal <priyanka0701m@gmail.com>
2018-09-13 21:05:41 +0530

    Create sumListElements.py

---
+++ sumListElements.py
@@ -0,0 +1,13 @@
+def sumListElements(L):
+    '''
+    Objective: To find the sum of all the elements of a given list.
+    Input:
+        L: A list of integers.
+    Output:
+        The sum of all the elements of the list.
+    '''
+    # Approach: sumListElements(L) = L[0] + sumListElements(L[1:])
+    if L == []:
+        return 0;
+    else:
+        return L[0] + sumListElements(L[1:])
